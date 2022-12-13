c442e391db1fb51a89117573f3aea77f1bcfc897
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2022-12-13 10:16:49 -0300

    feat: adicionando outros stress na API

--- index.js
+++ index.js
@@ -4,22 +4,33 @@ const express = require('express')
 const app = express()
 const routes = express.Router()
 
-routes.get('/cpu/stress', (req, res) => {
-  exec('stress-ng --cpu 0 --cpu-method all -t 1m', (error, stdout, stderr) => {
-    return res.send(stdout).status(200)
+function execCmd(cmd) {
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(error)
+    }
+    if (stderr) {
+      console.error(stderr)
+    }
+
+    console.log(stdout)
   })
+}
+
+routes.get('/cpu/stress', (req, res) => {
+  execCmd('stress-ng --cpu 0 --cpu-method all -t 1m')
 })
 
 routes.get('/memory/stress', (req, res) => {
-
+  execCmd('stress-ng --vm 8 --vm-bytes 80% -t 1m')
 })
 
 routes.get('/io/stress', (req, res) => {
-
+  execCmd('stress-ng --iomix 2 --iomix-bytes 50% -t 1m')
 })
 
 routes.get('/stress', (req, res) => {
-
+  execCmd('stress-ng --all 4 -t 1m')
 })
 
 app.use(routes)

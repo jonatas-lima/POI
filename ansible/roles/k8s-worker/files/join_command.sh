0067e2bbb10659f176f97dafdfd8d22a6687a0b5
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2022-12-26 17:32:25 -0300

    feat: adicionando instalação do prometheus via ansible pelo repositorio https://github.com/cloudalchemy/ansible-prometheus/tree/4.0.0 feat(prometheus): abrindo a porta 9090 no security group

--- ansible/roles/k8s-worker/files/join_command.sh
+++ ansible/roles/k8s-worker/files/join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.1.87:6443 --token jroj9n.ki0uot8edigq8j8u --discovery-token-ca-cert-hash sha256:75f12928e77e427f1a1a98f73087a518cf71e7448df837f26505a12dcbad1f6f 
+kubeadm join 192.168.1.66:6443 --token h34djs.avztyxjh8c52goho --discovery-token-ca-cert-hash sha256:a94d29fccc07b56dc8c3d47e9060d03494f5874955c19d19c324207b278dc68b 
--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -44,6 +44,13 @@ resource "aws_security_group" "POI-SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 9090
+    to_port = 9090
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0

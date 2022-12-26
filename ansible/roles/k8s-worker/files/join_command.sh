e0f06055d7643ff86adb46e1b7f5e692212880d6
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2022-12-26 18:47:45 -0300

    melhorias no playbook

--- ansible/roles/k8s-worker/files/join_command.sh
+++ ansible/roles/k8s-worker/files/join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.1.66:6443 --token h34djs.avztyxjh8c52goho --discovery-token-ca-cert-hash sha256:a94d29fccc07b56dc8c3d47e9060d03494f5874955c19d19c324207b278dc68b 
+kubeadm join 192.168.1.186:6443 --token k9av9p.cp57g0fkaa60i8g7 --discovery-token-ca-cert-hash sha256:fe7d506c0512fd0f9a2398412a8bd822697eca5c8f4672b79d54df658279b921 

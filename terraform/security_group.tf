5304668a82199749c8174443399a5e6a1ff7b9b7
Jonatas Ferreira <jonatas.lima@ccc.ufcg.edu.br>
2023-02-01 23:06:27 -0300

    usando zabbix e grafana

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -3,10 +3,24 @@ resource "aws_security_group" "POI-SG" {
   name   = "POI-SG"
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = [aws_subnet.POI-PUBLIC-SUBNET.cidr_block]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -38,7 +52,14 @@ resource "aws_security_group" "POI-SG" {
   }
 
   ingress {
-    from_port   = 0
+    from_port   = 10050
+    to_port     = 10050
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
     to_port     = 0
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
